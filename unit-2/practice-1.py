529295d5713dfbdd72034846bba33bc7868908f0
Abdul Al-Basith <abdul.albasith@gmail.com>
2020-03-24 13:19:54 -0400

    "Practice 1: Replace string with alphabet value"

---
+++ unit-2/practice-1.py
@@ -0,0 +1,13 @@
+alphabet = ["0","a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+    "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+    "w", "x", "y", "z"]
+position = ""
+
+string = input ('What would you like to "Encrypt": ')
+index = 0
+for letter in list(string):
+    if letter in alphabet:
+        alpha_numer = alphabet.index(letter)
+        position = position + str(alpha_numer)
+              
+print (position)
