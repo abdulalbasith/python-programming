8274cd9ba3f1d0750da76616c3e3835e8526554e
abdulalbasith <abdul.albasith@gmail.com>
2020-03-25 19:15:26 -0400

    Upper case solved

--- unit-2/practice-1.py
+++ unit-2/practice-1.py
@@ -1,13 +1,14 @@
-alphabet = ["0","a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
     "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
     "w", "x", "y", "z"]
 position = ""
 
 string = input ('What would you like to "Encrypt": ')
 index = 0
-for letter in list(string):
+for letter in list(string.lower()):
     if letter in alphabet:
-        alpha_numer = alphabet.index(letter)
-        position = position + str(alpha_numer)
+        alpha_number = alphabet.index(letter) + 1 
+        position = position + str(alpha_number)
               
 print (position)
+
