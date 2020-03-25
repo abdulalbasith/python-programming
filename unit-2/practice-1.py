088e3e7a33c551d602ef8915f53011488b50d463
Abdul Al-Basith <abdul.albasith@gmail.com>
2020-03-25 18:20:16 -0400

    c

--- unit-2/practice-1.py
+++ unit-2/practice-1.py
@@ -1,11 +1,12 @@
-alphabet = ["0","a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
     "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
     "w", "x", "y", "z"]
 position = ""
 
 string = input ('What would you like to "Encrypt": ')
-index = 1
+index = 0
 for letter in list(string):
+    index += 1
     if letter in alphabet:
         alpha_numer = alphabet.index(letter)
         position = position + str(alpha_numer)
