0feeaeaa33ed5d5af6e6a069c9efa850fa6932a7
Abdul Al-Basith <abdul.albasith@gmail.com>
2020-03-25 16:04:37 -0400

    a

--- unit-2/practice-2.py
+++ unit-2/practice-2.py
@@ -1,7 +1,11 @@
-list_of_numbers = [12,77,83,29,10,39,2,100,201,102]
+list_of_numbers = [12,77,83,29,10,39,2,100,200,267,75,98]
 index = 0
+largest_number = list_of_numbers [index]
 
-while list_of_numbers [index+1] > list_of_numbers [index]:
+
+while index < len(list_of_numbers):
+    if list_of_numbers [index] > largest_number:
+        largest_number = list_of_numbers [index]
     index += 1
 
-print (list_of_numbers [index])
+print (largest_number)
