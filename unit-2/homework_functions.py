4c5efc8d19e5a05202b92511ecba20d715891d0e
Abdul <abdul.albasith@gmail.com>
2020-03-28 10:58:36 -0400

    Finished problem 2, 3 and 4

--- unit-2/homework_functions.py
+++ unit-2/homework_functions.py
@@ -10,3 +10,41 @@ def letter_count (string, letter):
 
 print (letter_count("aaaabcde","a"))
 
+#Problem 2
+
+def count_words (string):
+    word_count = 1
+    index = 0
+    while index < len(string):
+        if string [index] == " ":
+            word_count += 1
+        index += 1
+    return word_count
+
+print (count_words("I'm staying home because of the epidemic"))
+
+#Problem 3
+
+def reverse_list (old_list):
+    new_list = []
+    for item in old_list:
+        new_list.insert(0,item)
+
+    return new_list
+
+print (reverse_list(["This", "is", "cool"]))    
+
+#Problem 4
+
+def split_list (integer_list,integer):
+    list_1 = []
+    list_2 = []
+    for item in integer_list:
+        if item < integer:
+            list_1.append(item)
+        if item >= integer:
+            list_2.append(item)
+    return list_1,list_2
+
+print (split_list([4, 5, 11, 8, 19], 10))
+
