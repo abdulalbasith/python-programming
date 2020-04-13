0564c4713235c4bb8f8f945a1ed416ec12222742
Abdul <abdul.albasith@gmail.com>
2020-04-13 11:08:42 -0400

    added compare to method

--- unit-5/movie_maker.py
+++ unit-5/movie_maker.py
@@ -6,9 +6,10 @@ class Movie:
         self.casts = casts 
         print (f'The movie, "{title}" is created')
     
-    def add_cast (self, cast={}):
+    def add_cast (self, cast):
         if "name" in cast and "age" in cast and "sex" in cast:
             self.casts.append(cast)
+            #print (f"Cast: {cast} added to {self.title}")
         else:
             return "Required fields are not supplied"
 
@@ -16,6 +17,12 @@ class Movie:
         print (f"Title: {self.title}\nGenre: {self.genre}\nRunning Time: {self.running_time} minutes\nCast: {self.casts}")
 
     def compare_to (self,Movie):
+        actors_in_common = 0
         for actor in self.casts:
-
-movie1 = Movie("")
+            if self.casts[actor] == Movie.casts[actor]:
+                actors_in_common += 1
+            if actors_in_common < 2:
+                return -1
+            else:
+                return 1
+        
