7dfd34ef0efaa259aaf55b351c9d2145ec081544
Abdul <abdul.albasith@gmail.com>
2020-04-13 14:00:19 -0400

    added compare_t and safe_to_file. Need to work on testing bothing methods.

---
+++ unit-5/movie_maker_test.py
@@ -0,0 +1,17 @@
+import movie_maker as m
+
+movie1 = m.Movie("Movie 1","Thriller",120)
+movie1.add_cast({"name":"Blinder", "age":32, "sex":"M"})
+movie1.add_cast({"name":"Jessica","age":33,"sex":"F"})
+movie1.add_cast({"name":"Donny", "age":37, "sex":"M"})
+
+movie2 = m.Movie("Movie 2","Thriller",120,[{"name":"Blinder", "age":32, "sex":"M"},{"name":"Anne","age":38,"sex":"F"},{"name":"Danny", "age":33, "sex":"M"}])
+
+'''
+movie2.add_cast({"name":"Blinder", "age":32, "sex":"M"})
+movie2.add_cast({"name":"Anne","age":38,"sex":"F"})
+movie2.add_cast({"name":"Danny", "age":33, "sex":"M"})
+'''
+movie1.describe()
+movie2.describe()
+m.Movie.save_to_file("MovieData")
